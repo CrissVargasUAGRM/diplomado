5a12347cbb2bd92c07038df2782cafbbdde1429b
cristhianBG <cristhian.vargas@bg.com.bo>
2024-01-04 17:15:59 -0400

    format spokless

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   '**/*.java': (filenames) => {
     return [`./gradlew :CheckInApi:spotlessApply -Pfiles=${filenames.join(',')}`,
+    `./gradlew :CheckIn.Application:spotlessApply -Pfiles=${filenames.join(',')}`,
     `./gradlew :CheckIn.Infraestructure:spotlessApply -Pfiles=${filenames.join(',')}`,
     `./gradlew :CheckInDomain:spotlessApply -Pfiles=${filenames.join(',')}`];
   },
